4350ab7f1d3c2b50efc9b904b5ff8184e7c76086
ROHAN VERMA <31581470+rohancse25@users.noreply.github.com>
2018-02-20 21:43:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
+from tkinter import *
 import pytube
-link=input()
-def show_progress(stream, chunk, file_handle, bytes_remaining):
-      if(bytes_remaining%100==0):
-           print(int(file_handle.tell()*100/(file_handle.tell()+bytes_remaining)),' %')
+class MyFirstGUI:
+    def __init__(self, master):
+        self.master = master
+        master.title("A simple GUI")
 
-yt = pytube.YouTube(link)
-yt.register_on_progress_callback(show_progress)
-vids= yt.streams.filter(subtype='mp4',progressive=True).all()
-for i in range(len(vids)):
-    print(i,'. ',vids[i])
-sno = int(input())
-vids[sno].download()
-print('done')
+        self.label = Label(master, text="WELCOME TO YOUTUBE DOWNLOADER")
+        self.label.pack()
+
+        self.l2 = Label(master, text="ENTER URL")
+        self.l2.pack()
+        self.tx1=Entry()
+        self.tx1.pack()
+        self.close_button = Button(master, text="DOWNLOAD", command=self.download)
+        self.close_button.pack()
+        self.close_button = Button(master, text="CLOSE", command=master.quit)
+        self.close_button.pack()
+
+    def download(self):
+        n = self.tx1.get()
+        yt = pytube.YouTube(n)
+        vids = yt.streams.filter(subtype='mp4', progressive=True).all()
+            
+        for i in range(len(vids)):
+            print(i, '. ', vids[i])
+        sno = int(input())
+        vids[sno].download()
+        print('done')
+root = Tk()
+my_gui = MyFirstGUI(root)
+root.mainloop()
